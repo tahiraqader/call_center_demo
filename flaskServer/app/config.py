e267be9f0ec946bc4245418ae05a5f2a245c071b
Tahira Qader <tahira.qader@gmail.com>
2025-04-29 23:55:21 -0700

    Add env variables for mongodb

--- flaskServer/app/config.py
+++ flaskServer/app/config.py
@@ -1,6 +1,9 @@
+
 import os
+from dotenv import load_dotenv
+
 
 class Config:
+    load_dotenv()  # looks for .env in the current dir
     #MONGO_URI = os.environ.get('MONGO_URI') or 'mongodb://localhost:27017/call_center'
-    MONGO_URI = 'mongodb://localhost:27017/call_center'
-
+    MONGO_URI = os.environ.get("MONGO_URI")
--- flaskServer/instance/config.py
+++
@@ -1,6 +0,0 @@
-import os
-
-class Config:
-    #MONGO_URI = os.environ.get('MONGO_URI') or 'mongodb://localhost:27017/call_center'
-        MONGO_URI = 'mongodb://localhost:27017/call_DB'
-
