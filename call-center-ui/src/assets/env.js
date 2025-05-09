d856a7a765f20f3d05facae96bcf4d9096f34451
Tahira Qader <tahira.qader@gmail.com>
2025-05-08 23:00:29 -0700

    Updated instructions to start flask server in Redme file

--- call-center-ui/src/assets/env.js
+++ call-center-ui/src/assets/env.js
@@ -1,6 +1,7 @@
 window["env"] = {
-    //apiUrl: "API_URL_PLACEHOLDER" // before runing, go to dist/assets/env.js and set the url correclty.
-    apiUrl:'http://ec2-3-238-94-159.compute-1.amazonaws.com:5000' //Change this the next time you run ec2
+    apiUrl: "API_URL_PLACEHOLDER" // before runing, go to dist/assets/env.js and set the url correclty.
+    //apiUrl:'http://ec2-3-239-58-162.compute-1.amazonaws.com:5000' 
+   
 
 }
 
